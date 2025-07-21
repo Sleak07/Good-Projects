6997b38484daeb13a9cd473931b245d0e1057e0b
Bimal Mallick <ghostridergothan8460@gmail.com>
2025-07-21 23:22:31 +0530

    Madlibs completed

--- Beginners/Mads/main.py
+++ Beginners/Mads/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from mads!")
+"""
+Madlibs is a game where user throws random verb or words
+without context .
+"""
+
+# Create a context
+# Add Verb,Noun etc
+# repeat it in loop
+
+story = (
+    "My friend and I spotted an {adj}.My friend decided to {verb1},I decided to {verb2}"
+)
+
+# input from user
+adj = input("Enter an adjective : ")
+verb1 = input("Enter an verb: ")
+verb2 = input("Enter another verb: ")
+
+# substitute the inputs
+story = f"My friend and I spotted an {adj} My friend decided to {verb1},I  decided to {verb2}"
 
 
-if __name__ == "__main__":
-    main()
+# print the output
+
+print(story)
