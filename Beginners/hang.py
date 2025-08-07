1151bb351ad8b7be28e7cb31208d4f233e15b6d2
Bimal Mallick <ghostridergothan8460@gmail.com>
2025-08-07 09:52:58 +0530

    game over

--- Beginners/hang.py
+++ Beginners/hang.py
@@ -17,6 +17,7 @@ A game in progress is shown below. In this game, the word to be guessed is hangm
 
 # Random choice of words from random.txt
 
+import string
 from random import choice
 
 
@@ -25,6 +26,174 @@ def select_word():
         word_list = words.readlines()
     return choice(word_list).strip()
 
-#Take the user input and validate it
-def get_player_input(guessed_word):
-   pass 
+
+# Take the user input and validate it
+def get_player_input(guessed_letter):
+    while True:
+        player_input = input("Enter your letter you guessed : ").lower()
+        if _validate_input(player_input, guessed_letter):
+            return player_input
+
+
+def _validate_input(player_input, guessed_letter):
+    return (
+        len(player_input) == 1
+        and player_input in string.ascii_lowercase
+        and player_input not in guessed_letter
+    )
+
+
+# To show the player he had letters guessed and how many are left
+def join_guessed_letter(guessed_letter):
+    return "".join(sorted(guessed_letter))
+
+
+# build the guessed_word to show the user
+def build_guessed_word(target_words, guessed_letter):
+    current_letters = []
+    for letter in target_words:
+        if letter in guessed_letter:
+            current_letters.append(letter)
+        else:
+            current_letters.append("_")
+    return "".join(current_letters)
+
+
+# draw hangman in ascii
+def draw_hanged_man(wrong_guesses):
+    hanged_man = [
+        r"""
+  -----
+  |   |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+  |   |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ |   |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+ ---  |
+/     |
+|     |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+ ---  |
+/   \ |
+|   | |
+      |
+-------
+""",
+    ]
+
+    print(hanged_man[wrong_guesses])
+
+
+# planning the end of game
+MAX_INCORRECT_GUESSES = 6
+
+
+def game_over(wrong_guesses, target_words, guessed_letter):
+    if wrong_guesses == MAX_INCORRECT_GUESSES:
+        return True
+    if set(target_words) <= guessed_letter:
+        return True
+
+    return False
+
+
+# Running and closing the loop
+if __name__ == "__main__":
+    # Initial setup
+    target_word = select_word()
+    guessed_letters = set()
+    guessed_word = build_guessed_word(target_word, guessed_letters)
+    wrong_guesses = 0
+    print("Welcome to Hangman!")
+    while not game_over(wrong_guesses, target_word, guessed_letters):
+        draw_hanged_man(wrong_guesses)
+        print(f"Your word is: {guessed_word}")
+        print(
+            "Current guessed letters: "
+            f"{join_guessed_letter(guessed_letters)}\n"
+        )
+
+        player_guess = get_player_input(guessed_letters)
+        if player_guess in target_word:
+            print("Great guess!")
+        else:
+            print("Sorry, it's not there.")
+            wrong_guesses += 1
+
+        guessed_letters.add(player_guess)
+        guessed_word = build_guessed_word(target_word, guessed_letters)
+
